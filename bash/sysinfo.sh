3b8e3a83f11567e9d080fcb145cb275ebbf13ff1
Joelu10 <122487091+Joelu10@users.noreply.github.com>
2023-02-18 19:57:36 -0500

    Update sysinfo.sh
    Added Comments for better readability.

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,8 +1,13 @@
  #!/bin/bash
+# Get fully-qualified domain name
 fqdn=$(hostname --fqdn)
+# Get host information
 host_info=$(hostnamectl)
+# Get IP addresses not on the 127 network
 ip_addrs=$(hostname -I | awk '$1 !~ /127./ {print $1}')
+# Get root filesystem status in human-friendly nature
 root_fs=$(df -h /)
+# Display output
 echo "FQDN: $fqdn"
 echo -e "Host Information:\n$host_info"
 echo "IP Addresses:$ip_addrs"
